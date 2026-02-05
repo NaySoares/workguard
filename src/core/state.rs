e67f4d8a9d66501bb47acd283c26bfb55781331c
Elienai Soares <elienay.soares07@gmail.com>
2026-02-05 11:06:56 -0300

    serialize WorkState

--- src/core/state.rs
+++ src/core/state.rs
@@ -1,15 +1,39 @@
 use chrono::NaiveTime;
+use serde::Serialize;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize)]
 pub enum WorkState {
     Working,
     Paused {
         reason: String,
+
+        #[serde(with = "time_format")]
         until: Option<NaiveTime>,
     },
     SoftBreak {
         label: String,
+        #[serde(with = "time_format")]
         until: Option<NaiveTime>,
     },
     Finished,
+}
+
+mod time_format {
+    use chrono::NaiveTime;
+    use serde::{self, Serializer};
+
+    const FORMAT: &str = "%H:%M:%S";
+
+    pub fn serialize<S>(
+        time: &Option<NaiveTime>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match time {
+            Some(t) => serializer.serialize_str(&t.format(FORMAT).to_string()),
+            None => serializer.serialize_none(),
+        }
+    }
 }
